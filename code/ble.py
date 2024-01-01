3fab715f89fe8cc9e19b7a00cd90fd4d46a05ba3
itay-mal <itay.mal@campus.technion.ac.il>
2024-01-01 20:12:54 +0200

    reconnecting client

--- code/ble.py
+++ code/ble.py
@@ -3,21 +3,11 @@ import signal
 from bleak import BleakScanner, BleakClient, BleakGATTCharacteristic
 import logging
 
-
-logging.basicConfig(format='%(asctime)s %(message)s', datefmt='[%H:%M:%S]')
+logging.basicConfig(format='[%(asctime)s @ %(funcName)20s] %(message)s', datefmt='%H:%M:%S')
 log = logging.getLogger()
 log.setLevel(logging.INFO)
 
 CHARACTARISTIC_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8"
-
-class DisconnectionException(Exception):
-    """Raised when device has disconnected."""
-    pass
-
-class ShutdownException(Exception):
-    """Raised when the program shuold shutdown."""
-    pass
-
 
 def ask_exit(_, __):
     global exit_flag
@@ -41,61 +31,49 @@ async def parse_data_cb(sender: BleakGATTCharacteristic, data:bytearray):
     timestamp,accX, accY, accZ, gyroX, gyroY, gyroZ = data.decode('utf-8').rstrip('#').split(':')
     log.info(f'timestamp:{timestamp}, accX:{accX}, accY:{accY}, accZ:{accZ}, gyroX:{gyroX}, gyroY:{gyroY}, gyroZ:{gyroZ}')
 
-async def notify_and_record(client: BleakClient):
-    global exit_flag
+
+def disconnect_cb(client: BleakClient):
     global connected
-    connected = await client.is_connected()
+    connected = False
+    log.error(f"Client with address {client.address} got disconnected!")
 
-    def disconnect_callback(client: BleakClient):
-        log.error(f"Client with address {client.address} got disconnected!")
-        global connected 
-        connected = False
-        # if not exit_flag:
-        #     log.error("Disconnection wa without request, trying to reconnect")
-        # raise DisconnectionException(f"Client with address {client.address} got disconnected!")
-
-    client.set_disconnected_callback(disconnect_callback)
-
-    log.info(f"Connected: {connected}")
+async def connect_and_start_notify(_client: BleakClient, uuid: BleakGATTCharacteristic = CHARACTARISTIC_UUID, _callback = parse_data_cb):
+    global connected
+    log.info(f"Connecting to device {_client.address}.")
+    await _client.connect()
+    connected = _client.is_connected
     log.info("Starting notification.")
-    await client.start_notify(CHARACTARISTIC_UUID, parse_data_cb)
-    while not exit_flag:
-        if not connected:
-            log.error("disconnected withou request")
-            await client.connect()
-            await client.start_notify(CHARACTARISTIC_UUID, parse_data_cb)
-        await asyncio.sleep(1)
-
-    log.info("Shutting Down.")
-    raise ShutdownException
+    await _client.start_notify(uuid, _callback)
 
 async def stay_connected(address: str, name: str, timeout: float = 4.0):
     global exit_flag
     exit_flag = False
+    global connected
+    connected = False
     log.info("Starting Loop")
-    client = BleakClient(address_or_ble_device=address, timeout=timeout)
-    try:
-        log.info(f"Connecting to device {name}.")
-        await client.connect()
-        await notify_and_record(client)
-    except DisconnectionException as e:
-        log.error(e)
-    except ShutdownException as e:
-        log.error(e)
-    except Exception as e:
-        log.error(e)
-        pass
+    while not exit_flag:
+        if not connected:
+            try:
+                client = BleakClient(address_or_ble_device=address, timeout=timeout, disconnected_callback=disconnect_cb)
+                await connect_and_start_notify(_client=client, uuid=CHARACTARISTIC_UUID, _callback=parse_data_cb)
+            except Exception as e:
+                log.error(f"{type(e)}: {e}")
+        log.info("connected: {}".format(client.is_connected))
+        await asyncio.sleep(1)
+    log.info("Exit Loop")
 
-    log.info("Shutting down notification.")
-    await client.stop_notify(CHARACTARISTIC_UUID)
-    log.info("Done shutting down notification.")
+    if client.is_connected:
+        log.info("Client is connected, shutting down gracefully.")
+        log.info("Shutting down notification.")
+        await client.stop_notify(CHARACTARISTIC_UUID)
+        log.info("Done shutting down notification.")
 
-    log.info("Disconnecting to device.")
-    await client.disconnect()
-    log.info("End of Loop Iteration")
+        log.info("Disconnecting to device.")
+        await client.disconnect()
+        log.info("Client Disconnected")
 
 async def run():
-    available_devices = await (find_devices())
+    available_devices = await find_devices()
     my_device = None
     for d in available_devices:
         if d.name == 'ESP32':
